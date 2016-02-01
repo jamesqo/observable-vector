2fad7fe73f5db3ec4e473ec999d74911d9d38990
James Ko <jamesqko@gmail.com>
2016-02-01 00:39:37 -0500

    Update the AssemblyInfos

--- src/ObservableVector/Properties/AssemblyInfo.cs
+++ src/ObservableVector/Properties/AssemblyInfo.cs
@@ -6,7 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ObservableVector")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(@"Observable Vector makes it easy to use the IObservableVector<T> interface in your Windows app.
+
+Find us on GitHub at https://github.com/jamesqo/observable-vector")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ObservableVector")]
@@ -24,6 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
