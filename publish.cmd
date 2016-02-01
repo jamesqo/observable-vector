18e38c10aae48907ce3f599d4f45993eca7b97ee
James Ko <jamesqko@gmail.com>
2016-02-01 00:33:41 -0500

    Complete most of the publish script

--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,25 @@
 @echo off
 setlocal
 
+:: Restore NuGet
+set "nugetdir=%~dp0bin"
+set "nuget=%nugetdir%\nuget.exe"
+:: Use v3 of the command line util, since v2 is still unable to work with PCLs
+set "nugeturl=http://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+
+if not exist "%nuget%" (
+    echo Restoring NuGet...
+    if not exist "%nugetdir%" mkdir "%nugetdir%"
+    powershell "iwr %nugeturl% -OutFile %nuget%"
+)
+
+:: Build the solution
 call "%~dp0build.cmd" -p "Any CPU" -c Release
+
+:: Create the packages
+cd "%~dp0src"
+for /d %%d in (*) do (
+    cd "%%d"
+    "%nuget%" pack %%d.csproj
+    cd "%~dp0src"
+)
